eff4c06e5792dbe06f8bef40af7ffdcd443c217b
lucas goeten <lucasgoetengithub@gmail.com>
2022-06-12 10:40:56 -0300

    Variaveis val vs var

---
+++ src/main/kotlin/com/fundamentos/Variaveis.kt
@@ -0,0 +1,18 @@
+package com.fundamentos
+
+fun main() {
+    var nome = "Lucas"
+    val nomeVal = "Lucas"
+
+    nome = "Julia"
+    // nomeVal = "Julia"
+
+
+}
+
+class variaveis(){
+    lateinit var teste: String
+    fun iniciarVariavel(){
+        teste = "teste"
+    }
+}
