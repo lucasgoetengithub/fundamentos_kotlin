619d3446dfbd6a01bc4d4c9ca632cef4c50ef43e
lucas goeten <lucasgoetengithub@gmail.com>
2022-06-12 10:50:07 -0300

    if else

---
+++ src/main/kotlin/com/fundamentos/IfElse.kt
@@ -0,0 +1,13 @@
+package com.fundamentos
+
+fun main(){
+
+}
+
+fun parOuImpar(numero: Int): String {
+    if (numero%2 == 0 ) {
+        return "Par"
+    } else {
+        return "Ímpar"
+    }
+}
